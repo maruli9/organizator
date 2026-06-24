420adb187fca6f578aacaa650c845ac983cf212c
maruli9 <118851819+maruli9@users.noreply.github.com>
2026-06-24 12:23:00 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,7 @@
-self.addEventListener("install", (event) => {
-  self.skipWaiting();
-});
+self.addEventListener("install",()=>self.skipWaiting());
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
-});
+self.addEventListener("activate",()=>self.clients.claim());
 
-/* 🔔 CLICK HANDLER (opcjonalnie przyszłość) */
-self.addEventListener("notificationclick", (event) => {
+self.addEventListener("notificationclick",event=>{
   event.notification.close();
-
-  event.waitUntil(
-    clients.matchAll({ type: "window", includeUncontrolled: true }).then(clientList => {
-      if (clientList.length > 0) {
-        clientList[0].focus();
-      }
-    })
-  );
-});
+});a
